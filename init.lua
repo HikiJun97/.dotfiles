471369fe4c725f22c429dd58d32f6c1434fab0d4
HikiJun97 <sgn04088@gmail.com>
2025-04-07 12:01:49 +0900

    init.lua: Add Golang config

--- init.lua
+++ init.lua
@@ -3,6 +3,13 @@
 
 -- Basic settings
 -- vim.opt.clipboard = "unnamedplus"
+vim.cmd([[
+    highlight Normal guibg=none
+    highlight NonText guibg=none
+    highlight Normal ctermbg=none 
+    highlight NonText ctermbg=none
+]])
+
 if vim.g.vscode then
 	print("Running in VSCode-neovim")
 	local vscode = require("vscode")
@@ -657,6 +664,9 @@ require("lazy").setup({
 		"loctvl842/monokai-pro.nvim",
 		config = function()
 			-- require("monokai-pro").setup({ filter = "default" })
+			require("monokai-pro").setup({
+				transparent_background = true,
+			})
 		end,
 	},
 	{ "patstockwell/vim-monokai-tasty" }, -- Monokai-tasty color scheme
@@ -847,6 +857,8 @@ local function RunCode()
 	elseif bufname:match("%.scss$") then
 		local filename = bufname:gsub("%.scss$", "")
 		RunSplitExecutor("sass", bufname, filename .. ".css")
+	elseif bufname:match("%.go$") then
+		RunSplitExecutor("go run", bufname, "")
 	end
 end
 
@@ -1012,6 +1024,7 @@ require("mason-lspconfig").setup({
 		"ts_ls",
 		"lua_ls",
 		"jsonls",
+		"gopls",
 		-- "prettier",
 		-- "debugpy",
 		-- "flake8"
@@ -1025,6 +1038,41 @@ require("mason-lspconfig").setup_handlers({
 			on_attach = lsp_attach,
 			autostart = true,
 		})
+	end,
+})
+
+lspconfig.gopls.setup({
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+			gofumpt = true,
+		},
+	},
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.go",
+	callback = function()
+		local params = vim.lsp.util.make_range_params()
+		params.context = { only = { "source.organizeImports" } }
+		-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+		-- machine and codebase, you may want longer. Add an additional
+		-- argument after params if you find that you have to write the file
+		-- twice for changes to be saved.
+		-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+		for cid, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+					vim.lsp.util.apply_workspace_edit(r.edit, enc)
+				end
+			end
+		end
+		vim.lsp.buf.format({ async = false })
 	end,
 })
 
